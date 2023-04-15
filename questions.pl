bf993c10fcf5966586acc1b5b572d2e2754e28c1
Godwill Afolabi <afolabigodwill24@gmail.com>
2023-04-15 00:52:58 -0500

    adding questions

--- questions.pl
+++ questions.pl
@@ -3,22 +3,12 @@
 Questions about your relations in another file in terms of defined rules in family_rules.
 */ 
  
-is_cousin() :- cousin(X, Y), write('Is '), write(X), write(' cousin to '), write(Y), write('?').
-
-who_grandchildren() :- grandparent(X, Y), write('Who are the grandchildren of '), write(X), write('?').
-
-who_ancestors() :- ancestor(X, Y), write('Who are the ancestors of '), write(Y), write('?').
-
-who_siblings() :- sibling(X, Y), write('Who are the siblings of '), write(Y), write('?').
-
-is_grandfather() :- grandfather(X, Y), write('Is '), write(X), write(' the grandfather of '), write(Y), write('?').
-
-who_daughters() :- parent(X, Y), female(Y), write('Who are the daughters of '), write(X), write('?').
-
-is_brother() :- brother(X, Y), write('Is '), write(X), write(' the brother of '), write(Y), write('?').
-
-who_nieces_nephews() :- sibling(P, X), (aunt(Y, P); uncle(Y, P)), write('Who are the nieces/nephews of '), write(X), write('?').
-
-who_female_children() :- parent(X, Y), female(Y), write('Who are the female children of '), write(X), write('?').
-
-who_siblings() :- sibling(X, Y), write('Who are the siblings of '), write(Y), write('?').
+is_cousin(X, Y) :- cousin(X, Y), write(X), write(' is a cousin of '), write(Y), nl.
+is_grandchildren(X, Y) :- grandparent(X, Y), write(X), write(' is a grandparent of '), write(Y), nl.
+is_ancestor(X, Y) :- ancestor(X, Y), write(X), write(' is an ancestor of '), write(Y), nl.
+who_siblings(X, Y) :- sibling(X, Y), write(X), write(' and '), write(Y), write(' are siblings'), nl.
+is_grandfather(X, Y) :- grandfather(X, Y), write(X), write(' is the grandfather of '), write(Y), nl.
+who_female_children(X, Y) :- parent(X, Y), female(Y), write(Y), write(' is a female child of '), write(X), nl.
+is_brother(X, Y) :- brother(X, Y), write(X), write(' is the brother of '), write(Y), nl.
+who_nieces_nephews(P, X) :- sibling(P, X), (aunt(X, P); uncle(X, P)), write(X), write(' is a niece or nephew of '), write(P), nl.
+who_children(X, Y) :- parent(X, Y), write(Y), write(' is a child of '), write(X), nl.
