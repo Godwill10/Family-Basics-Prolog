f76b5ff985ac2138c11777d385b07b8899d1c4ef
Godwill Afolabi <afolabigodwill24@gmail.com>
2023-04-20 00:27:55 -0500

    seperate second family facts

--- family_facts.pl
+++ family_facts.pl
@@ -48,26 +48,3 @@ born(ben, 1993).
 born(dave, 1970).
 born(jane, 1973).
 born(emma, 2000).
-
-/* Second family facts */
-
-parent(jeff, tom).
-parent(jeff, lisa).
-parent(amy, tom).
-parent(amy, lisa).
-parent(tom, jenny).
-parent(tom, michael).
-parent(lisa, sarah).
-parent(lisa, chris).
-
-married(jeff, amy).
-
-male(jeff).
-male(tom).
-male(michael).
-male(chris).
-
-female(amy).
-female(lisa).
-female(jenny).
-female(sarah).
---
+++ second_family_facts.pl
@@ -0,0 +1,22 @@
+/* Second family facts */
+
+parent(jeff, tom).
+parent(jeff, lisa).
+parent(amy, tom).
+parent(amy, lisa).
+parent(tom, jenny).
+parent(tom, michael).
+parent(lisa, sarah).
+parent(lisa, chris).
+
+married(jeff, amy).
+
+male(jeff).
+male(tom).
+male(michael).
+male(chris).
+
+female(amy).
+female(lisa).
+female(jenny).
+female(sarah).
